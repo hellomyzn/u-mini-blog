97fb9a819701f09cc1b48c30244d1f0ce9170dd9
hellomyzn <em711th@icloud.com>
2023-01-15 14:03:21 +1100

    add: user_id on blog db

--- backend/database/factories/BlogFactory.php
+++ backend/database/factories/BlogFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+use App\Models\User;
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
  */
@@ -17,6 +19,7 @@ class BlogFactory extends Factory
     public function definition()
     {
         return [
+            'user_id' => User::factory(),
             'title' => $this->faker->realText(20),
             'body' => $this->faker->realText(100),
             'updated_at' => $this->faker->dateTimeBetween('-10days', '0days'),
--- backend/database/migrations/2023_01_15_101713_create_blogs_table.php
+++ backend/database/migrations/2023_01_15_101713_create_blogs_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
             $table->string('title');
             $table->text('body');
             $table->timestamps();
